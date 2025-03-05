342650aef65695ef565d3bc88ce8f742d4155e0f
nagaraj <nagarajspatil66@gmail.com>
2025-03-05 12:41:43 +0530

    basichello file added
    done succesfully

---
+++ basichello.java
@@ -0,0 +1,15 @@
+package com;
+
+import java.util.ArrayList;
+
+public class basichello {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al=new ArrayList<String>();
+		
+		al.add("nagu");
+		al.add("vilash");
+		System.out.println(al);
+	}
+}
